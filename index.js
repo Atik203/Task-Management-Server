ffedfcf6ed3be7b252d90e8f03e4b4e3f82ec25e
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-12-22 19:49:06 +0600

    deploy it

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://taskmanage.surge.sh/"],
     credentials: true,
   })
 );
